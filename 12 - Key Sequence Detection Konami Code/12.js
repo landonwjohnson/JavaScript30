b9cafc33bc8865d13ac225db1013d5a0b2475252
Landon <landonwjohnson@gmail.com>
2018-01-04 09:47:14 -0700

    added some styling to the header on both pages

--- 12 - Key Sequence Detection Konami Code/12.js
+++ 12 - Key Sequence Detection Konami Code/12.js
@@ -1,12 +1,14 @@
 const pressed = [];
 const konamiCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba'
+document.getElementById('correct').style.display = "none";
+
 
 window.addEventListener('keyup', (e) => {
     pressed.push(e.key);
     pressed.splice(-konamiCode.length -1, pressed.length - konamiCode.length);
 
     if(pressed.join('').includes(konamiCode)){
-        alert('ding ding!')
+       document.getElementById('correct').style.display = "block";
     }
 
     console.log(pressed)
